5b820b022c9a82aba0b25b5fc1d6382112f6e022
Bruno Palma <b.palma@accenture.com>
2022-04-06 12:36:32 +0100

    added message to home

--- screens/home.jsx
+++ screens/home.jsx
@@ -5,15 +5,24 @@ import globalStyles from "../styles/global";
 const Home = () => {
   return (
     <View style={globalStyles.center}>
-      <Text style={styles.homeText}>Welcome to My Movies App</Text>
+      <Text style={styles.homeTitle}>Welcome to My Movies App</Text>
+      <Text style={styles.homeParagraph}>
+        Feel free to navigate through the tab below to start searching movies
+        and add them to your favorites list and check it.
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  homeText: {
+  homeTitle: {
     fontWeight: "bold",
     fontSize: 40,
+    marginTop: 20,
+    textAlign: "center",
+  },
+  homeParagraph: {
+    fontSize: 20,
     marginTop: 20,
     textAlign: "center",
   },
